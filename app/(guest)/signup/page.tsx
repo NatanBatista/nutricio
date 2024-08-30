f6487f1020b7ab788598f6aaf83f341596316f18
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2024-08-30 11:21:03 -0300

    feat: Pagina signUp

---
+++ app/(guest)/signup/page.tsx
@@ -0,0 +1,205 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client"
+
+import React from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+
+import { useRouter } from "next/navigation"
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+
+import { Input } from "@/components/ui/input"
+// import axios from "axios"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { useState } from "react"
+import { toast } from "@/components/ui/use-toast"
+
+const FormSchema = z.object({
+    email: z.string().email({
+        message: "Email incorreto."
+    }),
+    nickname: z.string().min(3, {
+        message: "Seu apelido deve ter pelo menos 3 caracteres"
+    }),
+    password: z.string().min(8, {
+        message: "A senha deve ter pelo menos 8 caracteres",
+    }),
+    password_confirmation: z.string().min(8, {
+        message: "A confirmação deve ter pelo menos 8 caracteres",
+    }),
+}).refine(data => data.password === data.password_confirmation, {
+    message: "As senhas não coincidem",
+    path: ["password_confirmation"]
+})
+
+const SignUp = () => {
+    const router = useRouter()
+
+    const [isLoading, setIsLoading] = useState(false)
+
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            email: "",
+            nickname: "",
+            password: "",
+            password_confirmation: "",
+        },
+    })
+
+    // async function onSubmit(data: z.infer<typeof FormSchema>) {
+    //     try {
+    //         setIsLoading(true)
+    //         const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth`, {
+    //             "email": data.email,
+    //             "nickname": data.nickname,
+    //             "password": data.password,
+    //             "password-confirmation": data.password_confirmation
+    //         })
+    //         router.push("/")
+    //         toast({
+    //             variant: "default",
+    //             title: "Conta criada com sucesso!",
+    //             description: `Um email com instruções de ativação foi enviado para ${
+    //                 response.data.data.email
+    //             }`
+    //         })
+    //     } catch (error: any) {
+    //         const errors = error.response.data.errors
+    //         if (errors.full_messages) {
+    //             errors.full_messages.forEach((message: string) => {
+    //                 toast({
+    //                     variant: "destructive",
+    //                     title: "Erro",
+    //                     description: message,
+    //                 })
+    //             })
+    //         }
+    //     } 
+    //     finally {
+    //         setIsLoading(false)
+    //     }
+
+    // }
+
+    return (
+        <div className="flex justify-center items-center my-10">
+            <Tabs defaultValue="account" className="w-[650px]">
+                <TabsList className="grid grid-cols-2">
+                    <TabsTrigger value="account">Conta</TabsTrigger>
+                    <TabsTrigger value="password">Senha</TabsTrigger>
+                </TabsList>
+                <Form {...form}>
+                    <form className="w-2/3 space-y-6">
+
+                        <TabsContent value="account">
+                            <Card className="w-[650px]">
+                                <CardHeader>
+                                    <CardTitle>
+                                        Conta
+                                    </CardTitle>
+                                </CardHeader>
+                                <CardContent>
+                                    <FormField
+                                        control={form.control}
+                                        name="email"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>E-mail</FormLabel>
+                                                <FormControl>
+                                                    <Input type="email" placeholder="e-mail" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+
+                                        )}
+                                    />
+                                    <FormField
+                                        control={form.control}
+                                        name="nickname"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Apelido</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder="apelido" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+
+                                        )}
+                                    />
+                                </CardContent>
+                            </Card>
+                        </TabsContent>
+                        <TabsContent value="password">
+                            <Card className="w-[650px]">
+                                <CardHeader>
+                                    <CardTitle>
+                                        Senha
+                                    </CardTitle>
+                                </CardHeader>
+                                <CardContent>
+                                    <FormField
+                                        control={form.control}
+                                        name="password"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Senha</FormLabel>
+                                                <FormControl>
+                                                    <Input type="password" placeholder="senha" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+
+                                        )}
+                                    />
+                                    <FormField
+                                        control={form.control}
+                                        name="password_confirmation"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel>Senha</FormLabel>
+                                                <FormControl>
+                                                    <Input type="password" placeholder="Confirme a senha" {...field} />
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+
+                                        )}
+                                    />
+                                    <div className="mt-4">
+                                    {isLoading ? (
+                                        <Button >
+                                            
+                                            Entrando...
+                                        </Button>
+                                    ) :
+                                        (
+                                            <Button type="submit">
+                                                Entrar
+                                            </Button>
+                                        )}
+                                    </div>
+                                </CardContent>
+                            </Card>
+                        </TabsContent>
+                    </form>
+                </Form>
+            </Tabs>
+        </div>
+    )
+}
+
+export default SignUp
