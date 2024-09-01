8fda6767e40725d58c9fd063c46a257e5f09f8a8
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2024-09-01 00:36:32 -0300

    feat: Exclusão de alimentos

--- app/(default)/alimentos/exclusion-button.tsx
+++ app/(default)/alimentos/exclusion-button.tsx
@@ -1,10 +1,46 @@
+"use client"
+
+import axios from "axios"
+import React, { useState } from "react"
+import { LoaderCircle } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { DialogClose } from "@radix-ui/react-dialog"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { useToast } from "@/components/ui/use-toast"
+import { ToastAction } from "@/components/ui/toast"
 
-import React from "react"
+interface ExclusionButtonProps {
+    id: number
+}
 
-const ExclusionButton = () => {
+const ExclusionButton: React.FC<ExclusionButtonProps> = ({
+    id
+}) => {
+    
+    const { toast } = useToast()
+    const [loading, setLoading] = useState<boolean>(false)
+
+    const deleteFood = async (id: number) => {
+        try {
+            setLoading(true)
+            await axios.delete('http://localhost:3001/foods/' + id)
+            toast({
+                description: "Item excluído com sucesso.",
+            })
+            setTimeout(() => {
+                window.location.reload();
+            }, 700);
+        } catch (error) {
+            toast({
+                variant: "destructive",
+                title: "Uh oh! Algo deu errado.",
+                description: "Houve um problema e não conseguimos deletar o alimento.",
+                action: <ToastAction altText="Tente novamente" onClick={() => deleteFood}>Tente novamente!</ToastAction>,
+              })
+        } finally {
+            setLoading(false)
+        }
+    }
 
     return (
         <>
@@ -19,7 +55,11 @@ const ExclusionButton = () => {
                     </DialogHeader>
 
                     <DialogFooter>
-                        <Button variant="destructive">Excluir </Button>
+                        {loading ?
+                        <Button className="w-24" variant="destructive"> <LoaderCircle className="mr-2 h-6 w-6 animate-spin" /> </Button> 
+                        :
+                        <Button className="w-24" onClick={() => deleteFood(id)} variant="destructive">Excluir </Button>
+                        }
                         <DialogClose asChild>
                             <Button variant="outline"> Cancelar </Button>
                         </DialogClose>
