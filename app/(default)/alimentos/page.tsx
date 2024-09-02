1c60792dee401b862d9ad45fdf69e6284e629e00
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2024-09-01 22:57:42 -0300

    feat: Adicionando tipo food

--- app/(default)/alimentos/page.tsx
+++ app/(default)/alimentos/page.tsx
@@ -15,44 +15,6 @@ import {
     PaginationPrevious,
 } from "@/components/ui/pagination"
 
-interface NutricionalValue {
-    id: number;
-    protein: number;
-    total_carbohydrate: number;
-    lipids: number;
-    dietary_fiber: number;
-    energy_kcal: number;
-    vitamin_a: number;
-    vitamin_c: number;
-    thiamine: number;
-    niacin: number;
-    vitamin_b6: number;
-    phosphorus: number;
-    iron: number;
-    sodium: number;
-    potassium: number;
-    calcium: number;
-    magnesium: number;
-    manganese: number;
-    copper: number;
-    zinc: number;
-    food_id: number;
-    created_at: string;
-    updated_at: string;
-}
-
-// Tipo para o alimento (food)
-interface Food {
-    id: number;
-    name: string;
-    table: string;
-    scientific_name: string;
-    code: string;
-    created_at: string;
-    updated_at: string;
-    nutricional_value: NutricionalValue;
-}
-
 const Alimentos = () => {
     const [foods, setFoods] = useState<Food[]>([]);
 
@@ -66,7 +28,7 @@ const Alimentos = () => {
             console.error('Error fetching data:', error);
         }
     }
-    
+
     useEffect(() => {
         fetchData(); // Chama a função fetchData quando o componente monta
     }, []);
---
+++ types/food/food.ts
@@ -0,0 +1,36 @@
+interface NutricionalValue {
+    id: number;
+    protein: number;
+    total_carbohydrate: number;
+    lipids: number;
+    dietary_fiber: number;
+    energy_kcal: number;
+    vitamin_a: number;
+    vitamin_c: number;
+    thiamine: number;
+    niacin: number;
+    vitamin_b6: number;
+    phosphorus: number;
+    iron: number;
+    sodium: number;
+    potassium: number;
+    calcium: number;
+    magnesium: number;
+    manganese: number;
+    copper: number;
+    zinc: number;
+    food_id: number;
+    created_at: string;
+    updated_at: string;
+}
+
+interface Food {
+    id: number;
+    name: string;
+    table: string;
+    scientific_name: string;
+    code: string;
+    created_at: string;
+    updated_at: string;
+    nutricional_value: NutricionalValue;
+}
