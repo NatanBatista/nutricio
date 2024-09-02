55f68fe3020d9a4a7010f3661dce3a8b0db3044d
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2024-09-02 20:28:38 -0300

    feat: Redirecionamento de acordo com a seção

--- app/(guest)/page.tsx
+++ app/(guest)/page.tsx
@@ -1,7 +1,15 @@
+import { auth } from "@/auth"
+import { redirect } from "next/navigation"
 import * as React from "react"
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
+
+  if (session) {
+    redirect('/alimentos')
+  }
   return (
-    <>  </>
+    <>
+    </>
   )
 }
