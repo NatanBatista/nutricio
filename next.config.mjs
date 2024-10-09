4f1bfd6d902be3181f908023db6b1233d5ab1c34
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2024-10-09 10:23:04 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: `http://nutricio-lb-1109550295.us-east-1.elb.amazonaws.com/:path*`,
+          },
+        ];
+      },
+};
 
 export default nextConfig;
